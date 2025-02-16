d4d2b05fda1c79b0fd93f97fb496e194ed6229e0
notrachelchen <chenrachel22@gmail.com>
2025-02-16 18:34:29 -0500

    lesson 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, number) {
+    if (number < 0) {
+        return 'ERROR';
+    }
+    
+    let final = '';
+    for (let i = 0; i < number; i++) {
+        final += word;
+    }
+    return final;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,21 @@
-const reverseString = function() {
+//strings are immutable in javascript 
 
+const reverseString = function(word) {
+    if (word.length == 0) {
+        return '';
+    }
+    let front = 0;
+    let back = word.length - 1;
+    let newWord = word.split('');
+    while (front < back) {
+        let temp = newWord[front];
+        newWord[front] = newWord[back];
+        newWord[back] = temp;
+        front += 1;
+        back -= 1;
+    }
+
+    return newWord.join('');
 };
 
 // Do not edit below this line
