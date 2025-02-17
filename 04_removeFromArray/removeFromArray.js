fb970e5cbdb18c1e1723cca2292bf2545ee60c17
notrachelchen <chenrachel22@gmail.com>
2025-02-16 19:12:40 -0500

    stil need to do 6 and 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(listOfNums, ...args) {
+    for (let i = 0; i < listOfNums.length; i++) {
+        if (args.includes(listOfNums[i])) {
+            listOfNums.splice(i, 1);
+            i--;
+        }
+    }
+    return listOfNums;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+// Implement a function that takes 2 positive integers and returns the sum of every integer between (and including) them:
 
+// ```javascript
+// sumAll(1, 4) // returns the sum of 1 + 2 + 3 + 4 which is 10
+// ```
+
+
+// test('works with larger number first', () => {
+//     expect(sumAll(123, 1)).toEqual(7626);
+//   });
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2) ) {
+        return "ERROR";
+    }
+    let total = 0;
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            total += i;
+        }
+        return total
+    } else {
+        for (let i = num2; i <= num1; i++) {
+            total += i;
+        }
+        return total
+    }
 };
 
 // Do not edit below this line
