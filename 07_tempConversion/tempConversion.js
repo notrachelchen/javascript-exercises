6ce37048ead31d902547895308a82779e0ef09a0
notrachelchen <chenrachel22@gmail.com>
2025-02-19 21:29:51 -0500

    problem 7 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+//convert farenheit to celsius 
+const convertToCelsius = function(tempInFaren) {
+  let curTemp = (tempInFaren - 32) * 5/9;
+  if (Number.isInteger(curTemp)) {
+    return Number(curTemp);
+  }
+  return Number(curTemp.toFixed(1)); 
 };
-
-const convertToFahrenheit = function() {
+//convert celsius to fahrenheit
+const convertToFahrenheit = function(tempInCel) {
+  let curTemp = tempInCel * 9/5 + 32;
+  if (Number.isInteger(curTemp)) {
+    return Number(curTemp);
+  }
+  return Number(curTemp.toFixed(1));
 };
 
 // Do not edit below this line
