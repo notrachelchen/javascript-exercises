17e2a5343b55c8f910db50eca0539d6749eaf89c
notrachelchen <chenrachel22@gmail.com>
2025-02-19 21:08:49 -0500

    finished problem 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+//Leap years 
+//every year divisible by 4
+//year is not divisible by 100
+//unless it is divisible by 400
+
+
+const leapYears = function(checkYear) {
+    if ((checkYear % 100 == 0) && (checkYear % 400 == 0)) {
+        return true;
+    } else if ((checkYear % 100 == 0) && (checkYear % 400 != 0)){
+        return false;
+    } else {
+        if ((checkYear % 4 == 0) && (checkYear % 100 != 0)) {
+            return true;
+        }
+    }
+    return false;
+
 
 };
 
